1a757b1de441f1c390fb184897942de797ac20ca
joshuaenoch <joshuaechow@gmail.com>
2024-07-28 20:38:42 -0700

    v1 done

--- chow-battle-mod/forge-1.20.1-47.3.0-mdk/src/main/java/com/example/chowbattle/item/Bungee.java
+++ chow-battle-mod/forge-1.20.1-47.3.0-mdk/src/main/java/com/example/chowbattle/item/Bungee.java
@@ -5,19 +5,27 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 
 public class Bungee extends Item {
+    private static final int COOLDOWN_TIME = 100;
+
     public Bungee(Properties properties) {
         super(properties);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
-
-        player.setDeltaMovement(0, 1, 0);
-
         ItemStack itemStack = player.getItemInHand(interactionHand);
-        return InteractionResultHolder.success(itemStack);
+
+        if (!player.getCooldowns().isOnCooldown(this)) {
+            player.setDeltaMovement(0, 1.2, 0);
+            player.getCooldowns().addCooldown(this, COOLDOWN_TIME);
+            return InteractionResultHolder.success(itemStack);
+
+        } else {
+            return InteractionResultHolder.fail(itemStack);
+        }
     }
 }
--- chow-battle-mod/forge-1.20.1-47.3.0-mdk/src/main/java/com/example/chowbattle/item/WindBoost.java
+++ chow-battle-mod/forge-1.20.1-47.3.0-mdk/src/main/java/com/example/chowbattle/item/WindBoost.java
@@ -11,18 +11,25 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 public class WindBoost extends Item {
+    private static final int COOLDOWN_TIME = 100;
     public WindBoost(Properties properties) {
         super(properties);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
-
-        Vec3 lookVector = player.getViewVector(1.0F);
-        Vec3 boostVector = lookVector.scale(2.0);
-        player.setDeltaMovement(player.getDeltaMovement().add(boostVector));
-
         ItemStack itemStack = player.getItemInHand(interactionHand);
-        return InteractionResultHolder.success(itemStack);
+
+        if (!player.getCooldowns().isOnCooldown(this)) {
+            Vec3 lookVector = player.getViewVector(1.0F);
+            Vec3 boostVector = lookVector.scale(2.0);
+            player.setDeltaMovement(player.getDeltaMovement().add(boostVector));
+
+            player.getCooldowns().addCooldown(this, COOLDOWN_TIME);
+            return InteractionResultHolder.success(itemStack);
+
+        } else {
+            return InteractionResultHolder.fail(itemStack);
+        }
     }
 }
